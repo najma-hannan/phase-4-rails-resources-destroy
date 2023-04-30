6d0d29c82cad0639cbe878b4255fb36a150229f2
Hannan <najma.mahat@student.moringaschool.com>
2023-04-30 10:53:38 +0300

    Completed rails resources

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  
   # GET /birds
   def index
     birds = Bird.all
@@ -39,6 +39,16 @@ class BirdsController < ApplicationController
     if bird
       bird.update(likes: bird.likes + 1)
       render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
